b5fa644aa65ab41881b8298a489599afd9d6b84f
BARBAPAPA215 <YUANHAOX@GMAIL.COM>
2020-01-09 14:48:07 +0100

    Update search.py

--- litstudy/search.py
+++ litstudy/search.py
@@ -118,12 +118,12 @@ def search_scopus(query, docs=None, retrieve_orcid=True):
                     for affiliation_id in author.affiliation:
                         if affiliation_id in affiliations_cache:
                             affiliation = affiliations_cache[affiliation_id]
-                    #     else:
-                    #        affiliation = ContentAffiliationRetrieval(affiliation_id)
-                    #         affiliations_cache[affiliation_id] = affiliation
-                    #     author_affiliations.append(Affiliation(name=affiliation.affiliation_name,
-                    #                                            city=affiliation.city,
-                    #                                            country=affiliation.country))
+                        else:
+                            affiliation = ContentAffiliationRetrieval(affiliation_id)
+                            affiliations_cache[affiliation_id] = affiliation
+                            author_affiliations.append(Affiliation(name=affiliation.affiliation_name,
+                                                               city=affiliation.city,
+                                                               country=affiliation.country))
         references = []
         if paper.refcount and int(paper.refcount) > 0 and paper.references:
             for reference in paper.references:
